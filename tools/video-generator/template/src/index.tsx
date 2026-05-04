83c270f260ccea489731d925618e04f2881d9bfc
r4ravi2008 <r4ravi2008@gmail.com>
2026-05-03 19:41:46 -0700

    fix: bundle Telugu font in video template

--- tools/video-generator/template/src/index.tsx
+++ tools/video-generator/template/src/index.tsx
@@ -1,4 +1,8 @@
 import { registerRoot } from 'remotion';
+import '@fontsource/noto-sans-telugu/400.css';
+import '@fontsource/noto-sans-telugu/600.css';
+import '@fontsource/noto-sans-telugu/700.css';
+import '@fontsource/noto-sans-telugu/800.css';
 import { RemotionRoot } from './Root';
 
 registerRoot(RemotionRoot);
